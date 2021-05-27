0a1fdab161781458c1fec591f095e8f9b79c66a5
Galli03 <84857932+Galli03@users.noreply.github.com>
2021-05-27 23:55:05 +0200

    State e Action
    aggiunta di State e Action

---
+++ minimax/minimax/tictactoe/Action.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class Action
+    {
+        public int row;
+        public int col;
+        public Action(int Row, int Col)
+        {
+            this.row = Row;
+            this.col = Col;
+        }
+        public int Row
+        {
+            get { return row; }
+        }
+        public int Col
+        {
+            get { return col; }
+        }
+    }
+}
---
+++ minimax/minimax/tictactoe/State.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace minimax.tictactoe
+{
+    class State
+    {
+        public static readonly int EMPTY = -1;
+        public static readonly int Circle = (int)Player.Circle;
+        public static readonly int Cross = (int)Player.Cross;
+
+        //public int[,] field;
+        //public Player CurrentPlayer;
+
+        public State()
+        {
+            field = new int[3, 3];
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    field[row, col] = EMPTY;
+                }
+            }
+            CurrentPlayer = Player.Cross;
+        }
+
+        public int[,] field { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public int getState(int row, int col)
+        {
+            int state;
+            if (field[row, col] == EMPTY)
+            {
+                state = EMPTY;
+            }
+            else if (field[row, col] == Circle)
+            {
+                state = Circle;
+            }
+            else
+            {
+                state = Cross;
+            }
+            return state;
+
+        }
+
+    }
+
+
+
+}
